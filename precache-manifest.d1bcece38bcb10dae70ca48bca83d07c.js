85a7cc9fe6357e2d9c076dbc1c167846cb7483ae
Nakka LeelaPrasanth <44019459+leelanakka@users.noreply.github.com>
2019-04-02 12:25:26 +0530

    Updates

---
+++ precache-manifest.d1bcece38bcb10dae70ca48bca83d07c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd4328c494124be36e16",
+    "url": "/tic-tac-toe-react/static/js/main.6132d7b2.chunk.js"
+  },
+  {
+    "revision": "c650f4249a1a9ac15f21",
+    "url": "/tic-tac-toe-react/static/js/runtime~main.27a0c5ca.js"
+  },
+  {
+    "revision": "08ca966498d955f1c83b",
+    "url": "/tic-tac-toe-react/static/js/2.3d8dcad6.chunk.js"
+  },
+  {
+    "revision": "056463dfe62e629861bcfaa8bc4b2be0",
+    "url": "/tic-tac-toe-react/index.html"
+  }
+];
